8504b954aa9e4af3bff87f710de32fc6dee1105c
Socialery <Socialery@users.noreply.github.com>
2014-09-03 03:41:09 +0930

    Parser update
    Now will also list albums with album names and image counts

--- parser.py
+++ parser.py
@@ -13,10 +13,14 @@ yaml.add_constructor(u'tag:yaml.org,2002:map',mapreplace)
 #listgrabber
 def imgur(album):
     headers = auth.imgur
-    response = requests.get("https://api.imgur.com/3/album/{}".format(album),headers=headers)
+    response = requests.get(
+        "https://api.imgur.com/3/album/{}".format(
+            album),headers=headers)
     response.raise_for_status()
-    time.sleep(2)
-    return list(response.json(object_hook=builtinext.AttrDict).data.images)
+    for x in filter(lambda x:x.startswith("X"),response.headers):
+        print("{0}: {1}".format(x,response.headers[x]))
+    time.sleep(0.5)
+    return response.json(object_hook=builtinext.AttrDict).data
 
 
 
@@ -30,30 +34,39 @@ with open('blurred.yaml') as f:
 output = []
 for item in dataset:
     output.append("### /u/{}".format(item.name))
-    if 'albums' in item:
-        ids = map(lambda x:"http://imgur.com/a/"+x.id,item.albums)
-        output.append("Albums: {}  ".format(', '.join(ids)))
+
     if 'threads' in item:
         ids = map(lambda x:"http://redd.it/"+x,item.threads)
         output.append("Threads: {}  ".format(', '.join(ids)))
     if 'images' in item:
         output.append("Images: http://imgur.com/{}  ".format(','.join(item.images)))
-    
-    output.append('')
 
     #games list
     if not 'games' in item:
         item.games = []
     if 'albums' in item:
+        output.append("Albums:")
+        output.append('')
         for album in item.albums:
+            ialbum=imgur(album.id)
+            print(album.id)
+            output.append("* [{0.title} ({0.images_count})]({0.link})".format(ialbum))
             if album.data:
-                for image in imgur(album.id):
-                    item.games.append("[{0.title}]({0.link})".format(image))
+                for image in ialbum.images:
+                    item.games.append(
+                        "[{0.title}]({0.link})".format(image))
+
+    output.append('')
+    output.append('Games:')
+    output.append('')
     for x in item.games:
         gamenum+=1
         output.append("* {}".format(x))
     output.append('')
-output.append('- - -')
+    output.append('- - -')
+    output.append('')
+
+
 output.append('total games: ~{}  \n'.format(gamenum))
 
 open("output.md","w").writelines(str(x)+"\n" for x in output)
